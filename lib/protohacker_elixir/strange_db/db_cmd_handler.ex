fa14f183a0a1cf83cb47718e9fdb0bee3ec5e52e
Keyang Li <liky1995@hotmail.com>
2026-02-27 23:10:48 +0800

    explain cmd

--- lib/protohacker_elixir/strange_db/db_cmd_handler.ex
+++ lib/protohacker_elixir/strange_db/db_cmd_handler.ex
@@ -1,2 +1,10 @@
 defmodule ProtohackerElixir.StrangeDb.DbCmdHandler do
+  alias ProtohackerElixir.StrangeDb.DbCmd
+
+  def handle_cmd(input) do
+    case String.split(input, "=", parts: 2) do
+      [key, value] -> %DbCmd{cmd: :insert, key: key, value: value}
+      [key] -> %DbCmd{cmd: :retrieve, key: key}
+    end
+  end
 end
