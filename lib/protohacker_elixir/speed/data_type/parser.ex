73b7508a29e69fe74e5afe212fca965d00449da3
Keyang Li <liky1995@hotmail.com>
2026-06-01 23:00:19 +0800

    1. try to parse messages

---
+++ lib/protohacker_elixir/speed/data_type/parser.ex
@@ -0,0 +1,24 @@
+defmodule ProtohackerElixir.Speed.DataType.Parser do
+  alias ProtohackerElixir.Speed.DataType.WantHeartbeat
+  alias ProtohackerElixir.Speed.DataType.Ticket
+  alias ProtohackerElixir.Speed.DataType.Plate
+  alias ProtohackerElixir.Speed.DataType.IAmDispatcher
+  alias ProtohackerElixir.Speed.DataType.IAmCamera
+  alias ProtohackerElixir.Speed.DataType.Heartbeat
+  alias ProtohackerElixir.Speed.DataType.Error
+
+  @type data_type ::
+          Error.t()
+          | Heartbeat.t()
+          | IAmCamera.t()
+          | IAmDispatcher.t()
+          | Plate.t()
+          | Ticket.t()
+          | WantHeartbeat.t()
+
+  @spec parse(binary) :: {:ok, {integer(), data_type()}} | {:error, term()}
+  def parse(<<type_of_msg::unsigned-8, size_of_msg::unsigned-8, real_msg::binary>>) do
+    <<first_n_bytes::binary-size(size_of_msg), rest_msg::binary>> = real_msg
+    {:error, :not_implemented}
+  end
+end
