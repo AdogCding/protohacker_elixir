b427c42be9213f28cb4f74de35f5a57f375e507a
Keyang Li <liky1995@hotmail.com>
2026-06-10 22:02:09 +0800

    dev p6 building server logic

--- lib/protohacker_elixir/speed/helper.ex
+++ lib/protohacker_elixir/speed/helper.ex
@@ -14,17 +14,15 @@ defmodule ProtohackerElixir.Speed.Helper do
   end
 
   @spec exceed_limit?(Witness.t(), float()) :: boolean()
-  def exceed_limit?(
-        %Witness{
-          mile1: mile1,
-          mile2: mile2,
-          timestamp1: timestamp1,
-          timestamp2: timestamp2
-        },
-        limit
-      ) do
-    distance = abs(mile2 - mile1)
-    interval = abs(timestamp2 - timestamp1) / 60 / 60
-    (distance / interval) |> round() > limit
+  def exceed_limit?(witness, limit) do
+    speed = calculate_speed(witness)
+    diff = speed - limit
+
+    if diff <= 0 do
+      false
+    else
+      # In cases where the car is exceeding the speed limit by less than 0.5 mph, it is acceptable to omit the ticket.
+      diff > 0.5 * 100
+    end
   end
 end
---
+++ lib/protohacker_elixir/speed/server.ex
@@ -0,0 +1,13 @@
+defmodule ProtohackerElixir.Speed.Server do
+  use GenServer
+
+  def start_link() do
+  end
+
+  def init(args) do
+  end
+
+  def handle_call(request, from, state) do
+	
+  end
+end
