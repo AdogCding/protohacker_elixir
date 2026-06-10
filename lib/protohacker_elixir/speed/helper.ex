8eb0e7d1e181dea8711600923502d04343ea4020
Keyang Li <liky1995@hotmail.com>
2026-06-10 21:38:51 +0800

    dev p6 helper functions

--- lib/protohacker_elixir/speed/helper.ex
+++ lib/protohacker_elixir/speed/helper.ex
@@ -1,7 +1,16 @@
 defmodule ProtohackerElixir.Speed.Helper do
   alias ProtohackerElixir.Speed.Witness
 
-  def calculate_speed(%Witness{}) do
+  @spec calculate_speed(Witness.t()) :: integer()
+  def calculate_speed(%Witness{
+        mile1: mile1,
+        mile2: mile2,
+        timestamp1: timestamp1,
+        timestamp2: timestamp2
+      }) do
+    distance = abs(mile2 - mile1)
+    interval = abs(timestamp2 - timestamp1) / 60 / 60
+    distance / interval * 100
   end
 
   @spec exceed_limit?(Witness.t(), float()) :: boolean()
